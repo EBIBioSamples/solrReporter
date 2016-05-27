dd1431a7b88b57a917ffc3e6399302ce632a7519
Kerruba <luca.cherubin@gmail.com>
2016-05-27 16:50:15 +0100

    Finish the merge with master (hopefully)

--- src/main/java/uk/ac/ebi/solrReporter/AppStarter.java
+++ src/main/java/uk/ac/ebi/solrReporter/AppStarter.java
@@ -1,24 +1,29 @@
 package uk.ac.ebi.solrReporter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
-import uk.ac.ebi.solrReporter.report.Report;
-import uk.ac.ebi.solrReporter.report.ReportData;
-import uk.ac.ebi.solrReporter.report.XMLReport;
-import uk.ac.ebi.solrReporter.sources.SourceFactory;
+        import org.slf4j.Logger;
+        import org.slf4j.LoggerFactory;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.boot.ApplicationArguments;
+        import org.springframework.boot.ApplicationRunner;
+        import org.springframework.boot.ExitCodeGenerator;
+        import org.springframework.stereotype.Component;
+        import uk.ac.ebi.solrReporter.report.Report;
+        import uk.ac.ebi.solrReporter.report.ReportData;
+        import uk.ac.ebi.solrReporter.report.XMLReport;
+        import uk.ac.ebi.solrReporter.sources.SourceFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.*;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.Set;
+        import java.util.concurrent.*;
 
 @Component
-public class AppStarter implements ApplicationRunner {
+public class AppStarter implements ApplicationRunner, ExitCodeGenerator {
     private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private XMLReport xmlReport;
 
     @Autowired
     private SourceFactory sourceFactory;
@@ -26,14 +31,13 @@ public class AppStarter implements ApplicationRunner {
     @Autowired
     private Report report;
 
-    @Autowired
-    private XMLReport xmlReport;
-
     private ExecutorService threadPool = null;
     private List<Future<?>> futures = new ArrayList<>();
 
-    @Value("${threadPoolCount}")
+    @Value("${threadPoolCount:4}")
     private int threadPoolCount;
+
+    private int exitCode = 0;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -41,6 +45,11 @@ public class AppStarter implements ApplicationRunner {
 
         threadPool = Executors.newFixedThreadPool(threadPoolCount);
         ReportData data = new ReportData();
+
+        data.setDBSource(sourceFactory.getDBSource().getSourceUrl());
+        data.setSolrSource("[" + sourceFactory.getSolrSource().getGroups() + ", " +
+                sourceFactory.getSolrSource().getSamples() + ", " +
+                sourceFactory.getSolrSource().getMerged() + "]");
 
         try {
             futures.add(threadPool.submit(() -> data.setSamplesDB(sourceFactory.getDBSource().getSamplesAccessions())));
@@ -60,20 +69,26 @@ public class AppStarter implements ApplicationRunner {
 
         } catch (InterruptedException | ExecutionException e) {
             log.error("Error collecting report data.", e);
-            System.exit(1);
+            exitCode = 1;
         }
 
-//        log.info(data.toString());
-        log.info(data.toString());
+        log.debug(data.toString());
 
         Boolean xmlReportOk = xmlReport.generateReport(data);
         Boolean reportOK = report.generateReport(data);
 
 
         if (reportOK && xmlReportOk) {
-            log.info("Report generated successfully!");
+            log.info("Report generated with no errors.");
         } else {
-            log.error("Failed to generate report!");
+            log.error("There are errors in the report.");
+            exitCode = 1;
         }
     }
+
+    @Override
+    public int getExitCode() {
+        return exitCode;
+    }
 }
+
